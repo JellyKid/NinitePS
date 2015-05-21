95e2fb8b8b15b6cc12ee5388c4a403a150ccc61a
JellyKid <seaoftea@gmail.com>
2015-05-21 14:14:58 -0400

    Adding in Build report and send mail ps1 to commit

---
+++ buildreport.ps1
@@ -0,0 +1,29 @@
+﻿
+
+$Header = @"
+<style>
+TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
+TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
+TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
+tr:nth-child(odd) {background: #CCC}
+</style>
+"@
+$date = Get-Date
+$pre = @"
+<h1>
+3rd Party Update Report 
+</h1>
+<h2>
+Run on $date
+</h2>
+"@
+$cnu = 4
+$Post = @"
+<h2>
+$cnu computers need updates
+</h2>
+"@
+
+$MyReport = Import-Csv .\ComputerList.csv | Sort-Object UpToDate
+
+$MyReport | Select 'Name','UpToDate','Pingable','LastContact','Needed' | ConvertTo-HTML -Head $Header -PreContent $Pre -PostContent $Post | Out-File UpdateReport.html
---
+++ email_results.ps1
@@ -0,0 +1,14 @@
+$mailprefs = @{
+            'To'           = 'jsommerville@bayportstatebank.com';
+            'Subject'      = '3rd Party Update Report';
+            'Body'         = 'Report Attached';
+            'From'         = 'ninite@bayportstatebank.com';
+            'SmtpServer'   = 'bayportstatebank-com.mail.protection.outlook.com'
+}
+
+if(Test-Path .\UpdateReport.html) {
+    $body = Get-Content .\UpdateReport.html | Out-String
+    $mailprefs.Set_Item('Body',$body)
+    $mailprefs.Add('BodyAsHtml',$true)
+    Send-MailMessage @mailprefs
+}
