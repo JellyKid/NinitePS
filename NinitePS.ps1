d18b47a4cc5eda65912043341a424edbb45a39e3
JellyKid <seaoftea@gmail.com>
2015-05-29 15:27:48 -0400

    Stopped exclude function from return wrong data type. Changed combine function to include. Added error reporting if ninte job fails completely.

--- NinitePS.ps1
+++ NinitePS.ps1
@@ -15,6 +15,14 @@ param(
 )
 
 
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
+
 #Get all machines in AD and test their connectivity
 
 Push-Location $PsScriptRoot
@@ -74,7 +82,7 @@ if($product){
 #--- End setting job options
  
 #--- Start helper functions
-function combine($string1, $string2) {
+function include($string1, $string2) {
 	$array1 = {$string1 -split ','}.Invoke()
 	$array2 = {$string2 -split ','}.Invoke()
 	foreach ($item in $array2) {
@@ -90,7 +98,7 @@ function exclude($string1, $string2) {
 	$array2 = {$string2 -split ','}.Invoke()
 	foreach ($item in $array2) {
 		if ($array1 -contains $item) {
-			$array1.remove($item)
+			$array1.remove($item) | out-null
 		}
 	}
 	return ($array1 -join ',')
@@ -158,13 +166,13 @@ function Call-Ninite {
 		$job += @("/remote",$computer)
 		Write-Host $job
 		$status = & .\Ninite.exe $job | Write-Output
-        
-		$status = $status[1..($status.Count - 1)]  
-		return $status
 	} else {
 		Write-Error "Error Ninite.exe doesn't exist in this path"
 		return $null
 	}
+	
+	return $status
+	
 }
 
 function BuildReport ($MyReport,$title) {
@@ -184,7 +192,7 @@ if ($audit) {
 	
 	$footer = "$cnu computers need updates"
 } else {
-	$header += "$jobname - $product"
+	$header += "$jobname - <span class=`"niniteproduct`">$product</span>"
 	$joberrors = 0
 	
 	foreach ($comp in $MyReport){
@@ -194,10 +202,12 @@ if ($audit) {
 	}
 	
 	if ($joberrors){
-		$footer = "Job completed with $joberrors errors, please see above"
+		$footer = "Job had $joberrors errors, please see above"
 	} else {
 		$footer = "Job completed successfully!"
 	}
+	
+	
 }
 
 
@@ -211,6 +221,7 @@ $pre = @"
 .ninitereport TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
 .ninitereport TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
 .ninitereport tr:nth-child(odd) {background: #CCC}
+.niniteproduct {text-transform: capitalize;}
 </style>
 
 <div class="ninitereport">
@@ -284,11 +295,27 @@ foreach ($computer in $ADList) {
 			
 			#Execute Ninite with specified parameters and return raw results
             $results = Call-Ninite $job $NewCompObj.Name
+			
+			#Check if job was successful 
+			if ($results) {
+				if ($results[0] -contains 'OK') {
+					$success = $results[0]
+					$results = $results[1..($results.Count - 1)]
+					
+				} else {
+					$success = $results[0]
+					foreach ($line in $results) {write-warning $line}	
+				} 
+			} else {
+				Write-Host 'No results?'
+				$NewCompObj.Error = 'No Results from last job'
+			}
             
           	#This next if/else block parses and stores the results in the 
 			#newly created computer object
-			if ($results) {	
-                
+			if ($success -eq 'OK') {	
+                				
+				
 				$ResultsHash = create_hash($results)
 				
 				#Change Status to Success so that results can be parsed properly
@@ -309,9 +336,11 @@ foreach ($computer in $ADList) {
 				}
 									
 			} else {
-				Write-Host 'No results?'
-				$NewCompObj.Error = 'No Results from last job'
+				$NewCompObj.Error = $success
+				$NewCompObj.JobStatus = "Failed"
 			}
+			
+			
 			
 			#Write out raw ninite reports, mainly for debugging purposes			
 			if($FullReports -and $results){
@@ -334,16 +363,18 @@ foreach ($computer in $ADList) {
 				$i = $ComputerStats.Name.IndexOf($NewCompObj.Name)
 				$ComputerStats[$i].Connectivity = $NewCompObj.Connectivity
 				$ComputerStats[$i].LastContact = $NewCompObj.LastContact
-				$ComputerStats[$i].Error = $NewCompObj.Error
-				<# $ComputerStats[$i].UpToDate = combine $ComputerStats[$i].UpToDate $NewCompObj.UpToDate
-				$ComputerStats[$i].UpdatesNeeded = combine $ComputerStats[$i].UpdatesNeeded $NewCompObj.UpdatesNeeded #>
+				
+				<# $ComputerStats[$i].UpToDate = include $ComputerStats[$i].UpToDate $NewCompObj.UpToDate
+				$ComputerStats[$i].UpdatesNeeded = include $ComputerStats[$i].UpdatesNeeded $NewCompObj.UpdatesNeeded #>
 				if ($uninstall) {
 					$ComputerStats[$i].UpToDate = exclude $ComputerStats[$i].UpToDate $NewCompObj.UpToDate
 					$ComputerStats[$i].UpdatesNeeded = exclude $ComputerStats[$i].UpdatesNeeded $NewCompObj.UpToDate
+					$ComputerStats[$i].Error = $NewCompObj.Error
 				}
 				if ($install -or $update) {
-					$ComputerStats[$i].UpToDate = combine $ComputerStats[$i].UpToDate $NewCompObj.UpToDate
+					$ComputerStats[$i].UpToDate = include $ComputerStats[$i].UpToDate $NewCompObj.UpToDate
 					$ComputerStats[$i].UpdatesNeeded = exclude $ComputerStats[$i].UpdatesNeeded $NewCompObj.UpToDate
+					$ComputerStats[$i].Error = $NewCompObj.Error
 				}
 				if ($audit) {
 					$ComputerStats[$i].UpToDate = $NewCompObj.UpToDate
